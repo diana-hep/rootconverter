f9e019cac6f5656862539f6605d43155b2517da6
Jim Pivarski <jpivarski@gmail.com>
2016-02-24 13:14:07 -0600

    uncommented

--- src/test.C
+++ src/test.C
@@ -13,14 +13,16 @@ void test() {
   //   std::cout << "  " << x << " " << y << " " << z << std::endl;
   // }
 
-  // TFile *file = TFile::Open("file:///home/pivarski/diana-github/scaroot/test-examples/verysimple.root");
-  // TTreeReader reader("ntuple", file);
-  // TTreeReaderValue<Float_t> x(reader, "x");
-  // TTreeReaderValue<Float_t> y(reader, "y");
-  // TTreeReaderValue<Float_t> z(reader, "z");
-  // while (reader.Next()) {
-  //   std::cout << *(x.Get()) << " " << *(y.Get()) << " " << *(z.Get()) << std::endl;
-  // }
+  std::cout << "file:///home/pivarski/diana-github/scaroot/test-examples/verysimple.root" << std::endl;
+
+  TFile *file1 = TFile::Open("file:///home/pivarski/diana-github/scaroot/test-examples/verysimple.root");
+  TTreeReader reader1("ntuple", file1);
+  TTreeReaderValue<Float_t> x(reader1, "x");
+  TTreeReaderValue<Float_t> y(reader1, "y");
+  TTreeReaderValue<Float_t> z(reader1, "z");
+  while (reader1.Next()) {
+    std::cout << *(x.Get()) << " " << *(y.Get()) << " " << *(z.Get()) << std::endl;
+  }
 
   // TFile *file = TFile::Open("file:///home/pivarski/diana-github/scaroot/test-examples/simple.root");
   // TTree *tree;
@@ -36,18 +38,20 @@ void test() {
   //   std::cout << "  " << one << " " << two << " " << three << std::endl;
   // }
 
-  TFile *file = TFile::Open("file:///home/pivarski/diana-github/scaroot/test-examples/simple.root");
-  TTreeReader reader("tree", file);
-  TTreeReaderValue<Int_t> one(reader, "one");
-  TTreeReaderValue<Float_t> two(reader, "two");
-  TTreeReaderArray<Char_t> three(reader, "three");
-  // TTreeReaderValue<TString*> fourfour(reader, "fourfour");
-  // TTreeReaderValue<std::string> four(reader, "four");
-  TTreeReaderArray<Float_t> five(reader, "five");
-  TTreeReaderArray<Float_t> six(reader, "six");
-  TTreeReaderArray<Float_t> seven(reader, "seven");
+  std::cout << "file:///home/pivarski/diana-github/scaroot/test-examples/simple.root" << std::endl;
+
+  TFile *file2 = TFile::Open("file:///home/pivarski/diana-github/scaroot/test-examples/simple.root");
+  TTreeReader reader2("tree", file2);
+  TTreeReaderValue<Int_t> one(reader2, "one");
+  TTreeReaderValue<Float_t> two(reader2, "two");
+  TTreeReaderArray<Char_t> three(reader2, "three");
+  // TTreeReaderValue<TString*> fourfour(reader2, "fourfour");
+  // TTreeReaderValue<std::string> four(reader2, "four");
+  TTreeReaderArray<Float_t> five(reader2, "five");
+  TTreeReaderArray<Float_t> six(reader2, "six");
+  TTreeReaderArray<Float_t> seven(reader2, "seven");
   // TTreeReaderValue<TObjArray> eight(reader, "eight");
-  while (reader.Next()) {
+  while (reader2.Next()) {
     std::cout << *(one.Get()) << " " << *(two.Get()) << " " << (char *)three.GetAddress() << std::endl;
     std::cout << "    ";
     for (int i = 0;  i < five.GetSize();  i++)
@@ -65,53 +69,30 @@ void test() {
     std::cout << std::endl;
   }
 
+  std::cout << "file:///home/pivarski/diana-github/scaroot/test-examples/notsosimple.root" << std::endl;
 
-  // Int_t one;
-  // Float_t two;
-  // Char_t three[10];
-  // TString four;
-  // Float_t five[2];
-  // Float_t six[4];
-  // Float_t seven[2][2];
-  // TObjArray eight;
-
-  // tree->Branch("one", &one, "one/I");
-  // tree->Branch("two", &two, "two/F");
-  // tree->Branch("three", &three, "three/C");
-  // tree->Branch("four", &four, 16000, 0);
-  // tree->Branch("five", &five, "five[2]/F");
-  // tree->Branch("six", &six, "six[one]/F");
-  // tree->Branch("seven", &seven, "seven[2][2]/F");
-  // tree->Branch("eight", &eight, 16000, 0);
-
-
-
-
-
-
-
-  // TFile *file = TFile::Open("file:///home/pivarski/diana-github/scaroot/test-examples/notsosimple.root");
-  // TTreeReader reader("tree", file);
-  // TTreeReaderValue<Double_t> scalar(reader, "scalar");
-  // TTreeReaderArray<Double_t> vector(reader, "vector");
-  // TTreeReaderArray<std::string> vector2(reader, "vector2");
-  // TTreeReaderArray<std::vector<Double_t> > tensor(reader, "tensor");
-  // while (reader.Next()) {
-  //   std::cout << *(scalar.Get()) << std::endl;
-  //   for (int i = 0;  i < vector.GetSize();  i++) {
-  //     std::cout << vector[i] << " ";
-  //   }
-  //   std::cout << std::endl;
-  //   for (int i = 0;  i < vector2.GetSize();  i++) {
-  //     std::cout << vector2[i] << " ";
-  //   }
-  //   std::cout << std::endl;
-  //   for (int i = 0;  i < tensor.GetSize();  i++) {
-  //     for (int j = 0;  j < tensor[i].size();  j++) {
-  //       std::cout << tensor[i][j] << " ";
-  //     }
-  //   }
-  //   std::cout << std::endl;
-  //   std::cout << std::endl;
-  // }
+  TFile *file3 = TFile::Open("file:///home/pivarski/diana-github/scaroot/test-examples/notsosimple.root");
+  TTreeReader reader3("tree", file3);
+  TTreeReaderValue<Double_t> scalar(reader3, "scalar");
+  TTreeReaderArray<Double_t> vector(reader3, "vector");
+  TTreeReaderArray<std::string> vector2(reader3, "vector2");
+  TTreeReaderArray<std::vector<Double_t> > tensor(reader3, "tensor");
+  while (reader3.Next()) {
+    std::cout << *(scalar.Get()) << std::endl;
+    for (int i = 0;  i < vector.GetSize();  i++) {
+      std::cout << vector[i] << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0;  i < vector2.GetSize();  i++) {
+      std::cout << vector2[i] << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0;  i < tensor.GetSize();  i++) {
+      for (int j = 0;  j < tensor[i].size();  j++) {
+        std::cout << tensor[i][j] << " ";
+      }
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+  }
 }
